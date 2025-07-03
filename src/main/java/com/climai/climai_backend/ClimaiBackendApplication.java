6c86a5b651d694a7b8e23b25ab0869790bdbe6fd
Fab-Ferreira <103126317+Fab-Ferreira@users.noreply.github.com>
2025-07-03 14:33:09 -0300

    Add files via upload

---
+++ src/main/java/com/climai/climai_backend/ClimaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.climai.climai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/climai/climai_backend/ClimaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climai.climai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
